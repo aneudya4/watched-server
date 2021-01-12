8e8442198480b4f948dd017ce789fd77961c3a8e
Aneudy Adames <a4aneudy@gmail.com>
2021-01-12 02:11:36 -0500

    testing

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DATABASE_URL || 'postgresql://aneudyadames@localhost/binge',
+    process.env.DATABASE_URL ||
+    'postgresql://dunder_mifflin:luigy@localhost/binge',
 };
