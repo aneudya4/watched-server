52017d32aade2029b313626abcd3b9d7d0bf9950
Aneudy Adames <a4aneudy@gmail.com>
2021-01-12 01:53:36 -0500

    testing

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const app = require('./app');
 
 const { PORT, DATABASE_URL } = require('./config');
-
+console.log(DATABASE_URL, 'AQUI DB');
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
